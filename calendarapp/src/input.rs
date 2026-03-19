33bb38d6d4139c553fbc3c3bc6f93caa4983e2a1
drussillo <daverussillo7@gmail.com>
2026-03-19 13:48:16 -0500

    Refactored input funciton and added year scrolling

--- calendarapp/src/input.rs
+++ calendarapp/src/input.rs
@@ -1,6 +1,7 @@
 use gtk4 as gtk;
 use gtk::prelude::*;
-use gtk::{glib, Widget};
+use gtk::{ glib };
+use gtk::gdk::{ Key, ModifierType };
 use std::rc::Rc;
 use std::cell::RefCell;
 
@@ -10,34 +11,40 @@ pub fn set_input(page: Rc<RefCell<calendar::Page>>) {
     let controller = gtk::EventControllerKey::new();
     let value = page.clone();
     controller.connect_key_pressed(move |_, key, _key_code, modifier_type| {
-        if key == gtk::gdk::Key::Escape {
-            std::process::exit(0);
-        }
-        if key == gtk::gdk::Key::L || (key == gtk::gdk::Key::l && modifier_type == gtk::gdk::ModifierType::CONTROL_MASK) {
-            value.borrow_mut().current_month += 1;
-            value.borrow().make_page();
-        }
-        if key == gtk::gdk::Key::H || (key == gtk::gdk::Key::h && modifier_type == gtk::gdk::ModifierType::CONTROL_MASK) {
-            value.borrow_mut().current_month -= 1;
-            value.borrow().make_page();
-        }
-        if key == gtk::gdk::Key::h {
-            value.borrow().window.emit_move_focus(gtk4::DirectionType::Left);
-        }
-        if key == gtk::gdk::Key::j {
-            value.borrow().window.emit_move_focus(gtk4::DirectionType::Down);
-        }
-        if key == gtk::gdk::Key::k {
-            value.borrow().window.emit_move_focus(gtk4::DirectionType::Up);
-        }
-        if key == gtk::gdk::Key::l {
-            value.borrow().window.emit_move_focus(gtk4::DirectionType::Right);
+
+        match(key, modifier_type) {
+            (Key::Escape, _) => std::process::exit(0),
+
+            (Key::L, _) | (Key::l, ModifierType::CONTROL_MASK) => {
+                value.borrow_mut().current_month += 1;
+                value.borrow().make_page();
+            }
+            (Key::H, _) | (Key::h, ModifierType::CONTROL_MASK) => {
+                value.borrow_mut().current_month -= 1;
+                value.borrow().make_page();
+            }
+            (Key::K, _) | (Key::k, ModifierType::CONTROL_MASK) => {
+                value.borrow_mut().current_month += 12; 
+                value.borrow().make_page();
+            }
+            (Key::J, _) | (Key::j, ModifierType::CONTROL_MASK) => {
+                value.borrow_mut().current_month -= 12; 
+                value.borrow().make_page();
+            }
+
+            (Key::h, _) => value.borrow().window.emit_move_focus(gtk4::DirectionType::Left),
+            (Key::j, _) => value.borrow().window.emit_move_focus(gtk4::DirectionType::Down),
+            (Key::k, _) => value.borrow().window.emit_move_focus(gtk4::DirectionType::Up),
+            (Key::l, _) => value.borrow().window.emit_move_focus(gtk4::DirectionType::Right),
+
+            (Key::r, _) => {
+                value.borrow_mut().reset_current_month();
+                value.borrow().make_page();
+            }
+
+            (_, _) => {}
         }
 
-        if key == gtk::gdk::Key::r {
-            value.borrow_mut().reset_current_month();
-            value.borrow().make_page();
-        }
         glib::Propagation::Proceed
     });
     page.borrow().window.add_controller(controller);
