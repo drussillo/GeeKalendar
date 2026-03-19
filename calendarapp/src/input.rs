4ae9766ab9c9d56948a5e7b7982103b2bc42f2a2
drussillo <daverussillo7@gmail.com>
2026-03-19 13:18:19 -0500

    Added vim page change

--- calendarapp/src/input.rs
+++ calendarapp/src/input.rs
@@ -9,15 +9,17 @@ use crate::calendar;
 pub fn set_input(page: Rc<RefCell<calendar::Page>>) {
     let controller = gtk::EventControllerKey::new();
     let value = page.clone();
-    controller.connect_key_pressed(move |_, key, _key_code, modifier_type| {
+    controller.connect_key_pressed(move |_, key, _key_code, _modifier_type| {
         if key == gtk::gdk::Key::Escape {
             std::process::exit(0);
         }
-        if key == gtk::gdk::Key::Right && modifier_type == gtk::gdk::ModifierType::CONTROL_MASK {
+        // if key == gtk::gdk::Key::Right && modifier_type == gtk::gdk::ModifierType::CONTROL_MASK {
+        if key == gtk::gdk::Key::L {
             value.borrow_mut().current_month += 1;
             value.borrow().make_page();
         }
-        if key == gtk::gdk::Key::Left && modifier_type == gtk::gdk::ModifierType::CONTROL_MASK {
+        // if key == gtk::gdk::Key::Left && modifier_type == gtk::gdk::ModifierType::CONTROL_MASK {
+        if key == gtk::gdk::Key::H {
             value.borrow_mut().current_month -= 1;
             value.borrow().make_page();
         }
